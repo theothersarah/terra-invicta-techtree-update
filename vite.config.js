916be997aa7ab63065bf9b02f986fc218c913672
Psilon <pzixel@gmail.com>
2025-04-06 01:41:17 +0300

    change base

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://pzixel.github.io/terra-invicta-techtree-update/'
 })
